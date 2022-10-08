681ddaa56f0a91f73b3beabe73e26e84fc4a73b9
KKS <kirill.kuliuckin2016@yandex.ru>
2022-10-08 07:43:35 +0300

    Delete JPA repo

--- src/main/java/repo/CoinIncomesJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package repo;
-
-import model.CoinIncome;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface CoinIncomesJpaRepository extends JpaRepository<CoinIncome, Long> {
-}
