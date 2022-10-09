6edac9ab9bb8de2e48737f3c50172126dd59bac2
Bogdan <cool.b987@yandex.ru>
2022-10-09 08:58:08 +0300

    Fixed swagger url

--- src/main/java/swagger/SwaggerConfig.java
+++ src/main/java/swagger/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 
     public static final Contact DEFAULT_CONTACT = new Contact(
             "Modern Making",
-            "",
+            "http://localhost:8091/",
             "");
 
     public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
